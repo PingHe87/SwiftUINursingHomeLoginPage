bcf8762b6edd339728f260e76901a322b2df1bb7
tjpeck23 <tjpeck@smu.edu>
2024-10-30 12:09:50 -0500

    Added calendar functions

--- NursingHomeLoginPage0927SwiftUI/NursingHomeLoginPage0927SwiftUI/Core/Calendar/CalendarModel.swift
+++ NursingHomeLoginPage0927SwiftUI/NursingHomeLoginPage0927SwiftUI/Core/Calendar/CalendarModel.swift
@@ -12,6 +12,7 @@ class CalendarModel: ObservableObject {
     private var store = EKEventStore()
     
     @Published var isAccessGranted: Bool = false
+    @Published var events: [EKEvent] = []
     
     init() {
         self.store = EKEventStore()
@@ -29,5 +30,49 @@ class CalendarModel: ObservableObject {
                 }
             }
         })
+    }
+    
+    // Method to fetch events between two dates
+    func fetchEvents(startDate: Date = Date(), endDate: Date = Calendar.current.date(byAdding: .month, value: 1, to: Date()) ?? Date()) {
+        
+        guard isAccessGranted else { return }
+        
+        let predicate = store.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        let fetchedEvents = store.events(matching: predicate)
+        
+        DispatchQueue.main.async {
+            self.events = fetchedEvents
+        }
+    }
+    
+    func createEvent(title: String, startDate: Date, endDate: Date, notes: String? = nil) {
+        guard isAccessGranted else { return }
+        
+        let event = EKEvent(eventStore: store)
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.notes = notes
+        
+        do {
+            try store.save(event, span: .thisEvent)
+        }catch {
+            print("Failed to save event: \(error.localizedDescription)")
+        }
+    }
+    
+    func deleteEvent(eventIdentifier: String) {
+        guard isAccessGranted else { return }
+        
+        if let event = store.event(withIdentifier: eventIdentifier) {
+            do {
+                try store.remove(event, span: .thisEvent)
+            } catch {
+                print("Failed to delete event: \(error.localizedDescription)")
+            }
+        }else {
+            print("Event not found with provided identifier: \(eventIdentifier)")
+        }
     }
 }
