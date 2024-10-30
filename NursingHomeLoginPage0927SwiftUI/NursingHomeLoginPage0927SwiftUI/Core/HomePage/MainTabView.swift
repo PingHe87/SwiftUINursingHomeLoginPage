44936767cc7cc086c80930788d856212db74e224
PingHe87 <heping.m87@gmail.com>
2024-10-30 14:21:18 -0500

    tabView added

--- NursingHomeLoginPage0927SwiftUI/NursingHomeLoginPage0927SwiftUI/Core/HomePage/MainTabView.swift
+++ NursingHomeLoginPage0927SwiftUI/NursingHomeLoginPage0927SwiftUI/Core/HomePage/MainTabView.swift
@@ -5,3 +5,68 @@
 //  Created by p h on 10/30/24.
 //
 
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab = 1  // Default to the Home tab with index 1
+
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(red: 236/255, green: 19/255, blue: 0, alpha: 1.0)  // Set background color
+        UITabBar.appearance().unselectedItemTintColor = .white  // Set color for unselected tab items
+    }
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            // Calendar Tab
+            Text("Calendar Page Placeholder")
+                .tabItem {
+                    Label("Calendar", systemImage: "calendar")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(selectedTab == 0 ? Color.red : Color.white)
+                        .background(selectedTab == 0 ? Color.white : Color.clear)
+                        .cornerRadius(10)
+                }
+                .tag(0)
+
+            // Weather Tab
+            WeatherView(weather: previewWeather)
+                .tabItem {
+                    Label("Weather", systemImage: "cloud.sun")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(selectedTab == 2 ? Color.red : Color.white)
+                        .background(selectedTab == 2 ? Color.white : Color.clear)
+                        .cornerRadius(10)
+                }
+                .tag(2)
+
+            // Homepage Tab in the center
+            Text("Homepage Placeholder")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(selectedTab == 1 ? Color.red : Color.white)
+                        .background(selectedTab == 1 ? Color.white : Color.clear)
+                        .cornerRadius(10)
+                }
+                .tag(1)
+
+            // Profile Tab on the right
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.crop.circle")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(selectedTab == 3 ? Color.red : Color.white)
+                        .background(selectedTab == 3 ? Color.white : Color.clear)
+                        .cornerRadius(10)
+                }
+                .tag(3)
+        }
+        .accentColor(.white)  // Set color for selected icons and text to white
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
